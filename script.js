e23fded1002b9c9af59f704e9f9a5000a10bab13
Singularity <phamtiengm@gmail.com>
2024-06-06 22:01:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchInput = document.getElementById("search");
+const pages = {
+  "trang chu": "Index.html",
+  "trangchu": "Index.html",
+  "trang chủ": "Index.html",
+  "lienhe": "contact.html",
+  "lien he": "contact.html",
+  "liên hệ": "contact.html",
+  "hoithao": "seminar.html",
+  "hoi thao": "seminar.html",
+  "hội thảo": "seminar.html",
+  "sport": "sport.html",
+  "video": "videos.html",
+  "videos": "videos.html",
+  "picture": "picture.html",
+  "tamnhin": "vision.html",
+  "tam nhin": "vision.html",
+  "tầm nhìn": "vision.html",
+  "trải nghiệm": "tnhn.html",
+  "trai nghiem": "tnhn.html",
+  "muc dich thanh lap": "mdtl.html",
+  "mục đích thành lập": "mdtl.html",
+  "hieu ca": "hieuca.html",
+  "hiệu ca": "hieuca.html",
+};
+
+searchInput.addEventListener("click", () => {
+  const input = document.getElementById("search-input").value.toLowerCase();
+
+  if (pages.hasOwnProperty(input)) {
+    window.location.href = pages[input];
+  } else {
+    alert("Không tìm thấy trang này.");
+  }
+})
